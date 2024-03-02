2fdc71e917a1f4a8cb37e92947e4d73fa9e2ddf5
Patryk 'pcpl2' Ławicki <kontakt@patryk-lawicki.pl>
2024-03-02 17:40:39 +0100

    Fixed path on bsd.

--- premakeProj/fmt/FMT.lua
+++ premakeProj/fmt/FMT.lua
@@ -8,7 +8,7 @@ project "LibFMT"
 	objdir "../../Obj/%{prj.name}/%{cfg.buildcfg}"
 	location "../../Out"
 
-	postbuildcommands { "{COPYDIR} %[%{!wks.location}vendor/fmt/include] %[%{!cfg.targetdir}/../include]" }
+	postbuildcommands { "{COPYDIR} %[%{!wks.location}/vendor/fmt/include] %[%{!cfg.targetdir}/../include]" }
 
 	includedirs {
 		"../../vendor/fmt/include/"
